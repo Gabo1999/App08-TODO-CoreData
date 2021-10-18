339d1bc748789d0ac26d1937a08b0ed5cc538b71
David Josué Marcial Quero <A00828702@itesm.mx>
2021-10-18 07:30:07 -0700

    Initial Commit

---
+++ App08-TODO-CoreData/App08_TODO_CoreDataApp.swift
@@ -0,0 +1,20 @@
+//
+//  App08_TODO_CoreDataApp.swift
+//  App08-TODO-CoreData
+//
+//  Created by David Josué Marcial Quero on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct App08_TODO_CoreDataApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
